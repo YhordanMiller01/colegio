35f92e250bedac445755c86a23f456e6fc97faed
Yhordan Miller <yhordanmiller@feetcolombia.com>
2025-09-12 22:41:05 -0500

    faature (sidebar) actualiza colores

--- client/src/components/ui/sidebar.tsx
+++ client/src/components/ui/sidebar.tsx
@@ -187,7 +187,7 @@ function Sidebar({
           data-sidebar="sidebar"
           data-slot="sidebar"
           data-mobile="true"
-          className="bg-sidebar text-sidebar-foreground w-[var(--sidebar-width)] p-0 [&>button]:hidden"
+          className="bg-black/90 text-white w-[var(--sidebar-width)] p-0 [&>button]:hidden"
           style={
             {
               "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
@@ -199,7 +199,7 @@ function Sidebar({
             <SheetTitle>Sidebar</SheetTitle>
             <SheetDescription>Displays the mobile sidebar.</SheetDescription>
           </SheetHeader>
-          <div className="flex h-full w-full flex-col">{children}</div>
+          <div className="flex h-full w-full flex-col text-white fill-white">{children}</div>
         </SheetContent>
       </Sheet>
     )
